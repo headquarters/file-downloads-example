0b3da77bce24370f2044cf5e46346e2d6e1f87ff
Michael Head <michaelehead@gmail.com>
2017-05-25 17:17:08 -0400

    Researching IE11 prompting downloads for PDFs

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 'use strict';
 var express = require('express');
 var path = require('path');
-
+var helmet = require('helmet');
 var app = express();
 
+app.use(helmet({
+  frameguard: false,
+  // will force IE11 to prompt the user to download a PDF instead of previewing it
+  ieNoOpen: false
+}));
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/iframe', function(req, res) {
+  res.sendFile(path.join(__dirname, 'iframe.html'));
+})
 
 app.get('/file', function(req, res) {
   res.download('static/sample.docx');
 });
 
+app.get('/view-file', function(req, res) {
+  const options = {
+    headers: {
+      'Content-Disposition': 'inline; filename="sample.pdf"'
+    }
+  };
+
+  res.sendFile(path.join(__dirname, 'static/sample.pdf'), options);
+});
+
 app.listen(5000, function() {
-    console.info('Server listening on port 5000');
+  console.info('Server listening on port 5000');
 });
